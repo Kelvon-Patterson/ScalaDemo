5d8595055be72716d32f89f5c7235b68da1163d0
Kelvon-Patterson <kelvonpatterson044@gmail.com>
2022-07-26 09:43:31 -0500

    feat: Scala Functions

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -1,3 +1,38 @@
 object Functions {
 
+object Math{
+  def add(x:Int , y: Int): Int = {
+    x+y;
+  }
+  def square(x: Int) = x*x;
+}
+
+  def add(x: Int, y: Int): Int = {
+  return  x + y;
+  }
+
+  def subtract(x: Int, y: Int): Int = {
+    x - y;
+  }
+
+  def multiply(x: Int, y: Int): Int = x * y;
+
+
+  def divide(x: Int, y: Int)  = x / y;
+
+//These are four basic different ways to write functions in scala
+
+
+  def main(args: Array[String]): Unit = {
+    println(Math.add(45,15))
+    println(Math square 5);
+
+    println(add(45, 15))
+    println(subtract(45, 15))
+    println(multiply(45, 15))
+    println(divide(45, 15))
+
+
+
+  }
 }
