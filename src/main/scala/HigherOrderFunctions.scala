64d812dac9e248073cc3e9026afc8234ac02df07
Kelvon-Patterson <kelvonpatterson044@gmail.com>
2022-07-26 11:42:23 -0500

    feat: higher order functions

---
+++ src/main/scala/HigherOrderFunctions.scala
@@ -0,0 +1,12 @@
+object HigherOrderFunctions {
+
+  def math(x : Double, y : Double, z:Double, f: ( Double, Double) => Double ): Double = f(f(x,y),z);
+
+  def main(args: Array[String]): Unit ={
+val result = math(50, 20, 10, (x,y) => x + y)
+    val result2 = math(50, 20, 10, _+_) //wildcard
+    println(result)
+    println(result2)
+  }
+
+}
